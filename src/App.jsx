e5ca8b2bc305ee59446089368d61ff065323e87b
Abhishek3821 <abhisheksinghchauhan97@gmail.com>
2026-01-23 06:55:44 +0530

    fixxxx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,31 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./Pages/Navbar"    
-import Home from "./Pages/Home";
+
+import Navbar from "./Pages/Navbar";
 import Footer from "./Pages/Footer";
+
+import Home from "./Pages/Home";
 import Services from "./Pages/Services";
 import AboutUs from "./Pages/AboutUs";
 import ContactUs from "./Pages/ContactUs";
 import Project from "./Pages/Project";
+
 function App() {
-
-
   return (
-    <>
-       <BrowserRouter>
+    <BrowserRouter>
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/aboutUs" element={<AboutUs />} />
+        <Route path="/about-us" element={<AboutUs />} />
         <Route path="/services" element={<Services />} />
         <Route path="/blog" element={<div>Blog</div>} />
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/project" element={<Project />} />
       </Routes>
+
       <Footer />
     </BrowserRouter>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
